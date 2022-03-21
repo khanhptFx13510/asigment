01a38f581f709546d12f413b5f053171b6682f72
khanhptFx13510 <khanhdudn@gmail.com>
2022-03-21 17:11:50 +0700

    commit lan 1

---
+++ asigment_1/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import { STAFFS } from './shared/staffs.jsx';
+import StaffList from './components/StaffListComponent.js';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      staffs: STAFFS,
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          
+        </Navbar>
+        <StaffList staffs={this.state.staffs} />
+      </div>
+    );
+  }  
+}
+
+
+export default App;
---
+++ asigment_1/src/components/StaffListComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class StaffList extends Component {
+   constructor(props) {
+      super(props);
+      console.log(this.props.staffs);
+   }
+   render() {
+      return (
+         <div>
+
+         </div>
+      )
+   }
+}
+export default StaffList;

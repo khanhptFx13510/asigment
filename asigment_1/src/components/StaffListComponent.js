1f68608d6019b6debbdeeed3c3f23e12a107290d
khanhptFx13510 <khanhdudn@gmail.com>
2022-03-21 18:56:12 +0700

    commit 4 finish render infor staff

--- asigment_1/src/components/StaffListComponent.js
+++ asigment_1/src/components/StaffListComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import dateFormat from 'dateformat'; 
-import { Button } from 'reactstrap'
+import { Button , Card , CardBody , CardText ,CardTitle } from 'reactstrap';
 
 class StaffList extends Component {
    constructor(props) {
@@ -20,7 +20,25 @@ class StaffList extends Component {
 
    renderSelectedStaff(staff) {
       if(staff != null){
-         
+         return (
+            <div className="row">
+               <Card className="col-12 col-md-6">
+                  <h3>Họ và tên: {staff.name}</h3>
+                  <CardBody>
+                     <CardText>Ngày sinh: {dateFormat(staff.doB , "dd/mm/yyyy")}</CardText>
+                     <CardText>Ngày vào công ty: {dateFormat(staff.startDate , "dd/mm/yyyy")}</CardText>
+                     {/* <CardText>Phòng ban: {staff.department}</CardText> */}
+                     <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+                     <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+                     <CardText>Hệ số lương: {staff.salaryScale}</CardText>
+                  </CardBody>
+
+
+               </Card>
+            </div>
+         )
+      } else{
+         return <div></div>
       }
    }
 
@@ -29,9 +47,9 @@ class StaffList extends Component {
          return (
             <div key={staff.id} className="col-12 col-sm-6 col-md-4 m-0.5 mb-2">
                <Button block size="lg" color="secondary"
-               onClick={() => onStaffSelect(staff)}>
-                  {staff.name}</Button>
-               
+               onClick={() => this.onStaffSelect(staff)}>
+                  {staff.name}
+               </Button>
             </div>
          )
             
@@ -42,6 +60,10 @@ class StaffList extends Component {
             <div className="row">
                {staffs}
             </div>
+            <div>
+               {this.renderSelectedStaff(this.state.selectedStaff)}
+            </div>
+
          </div>
             
 

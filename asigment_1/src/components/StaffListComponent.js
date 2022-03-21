36fa815d35aad1cdd963fd63a1f8d852c663c03e
khanhptFx13510 <khanhdudn@gmail.com>
2022-03-21 18:11:39 +0700

    commit 3 render name staff

--- asigment_1/src/components/StaffListComponent.js
+++ asigment_1/src/components/StaffListComponent.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import dateFormat from 'dateformat'; 
+import { Button } from 'reactstrap'
 
 class StaffList extends Component {
    constructor(props) {
       super(props);
-      console.log(this.props.staffs);
+      
+      this.state = {
+         selectedStaff: null
+      }
+
    }
+
+   onStaffSelect(staff) {
+      this.setState({ 
+         selectedStaff: staff
+      })
+   }
+
+   renderSelectedStaff(staff) {
+      if(staff != null){
+         
+      }
+   }
+
    render() {
       const staffs = this.props.staffs.map(staff => {
          return (
-            <div className="row">
+            <div key={staff.id} className="col-12 col-sm-6 col-md-4 m-0.5 mb-2">
+               <Button block size="lg" color="secondary"
+               onClick={() => onStaffSelect(staff)}>
+                  {staff.name}</Button>
                
             </div>
          )
+            
+            
       })
       return (
          <div className="container">
-            
+            <div className="row">
+               {staffs}
+            </div>
          </div>
+            
 
       )
    }

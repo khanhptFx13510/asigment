d6ee5ffe0d1efb841c88a453a122ad4cc1efbfbd
khanhptFx13510 <khanhdudn@gmail.com>
2022-03-31 20:38:05 +0700

    commit 3 tao homepage

--- asigment_2/src/components/HomePageComponent.js
+++ asigment_2/src/components/HomePageComponent.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem , CardBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 
 function HomePage(props){
    const staffs= props.staffs.map(staff =>{
       return(
-         <div></div>
+         <div key={staff.id} className="col-md-2 col-sm-4 col-6 my-2">
+            <Card>
+               <CardImg src={staff.image} width="100%" alt={staff.name} />
+               <CardBody>
+                  <CardTitle>{staff.name}</CardTitle>
+               </CardBody>
+            </Card>
+
+         </div>
       )
    });
    return (
       <div className="container">
          <div className="row">
-            <Breadcrumb>
-               <BreadcrumbItem active>
-                  <Link to="/nhanvien">Nhanvien</Link>
-               </BreadcrumbItem>
-               {/* <Breadcrumb active>nhanvien</Breadcrumb> */}
-            </Breadcrumb>
+            <h2>Nhân Viên</h2>
          </div>
+         <hr/>
+         <div className="row">
+            {staffs}
+         </div>   
       </div>
    )
 }
 
-export default HomePage
+export default HomePage;
--- asigment_2/src/components/MainComponent.js
+++ asigment_2/src/components/MainComponent.js
@@ -3,24 +3,30 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import HomePage from './HomePageComponent.js';
 
-import { STAFFS } from '../shared/staffs.jsx';
+import { STAFFS , DEPARTMENTS , ROLE } from '../shared/staffs.jsx';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         staffs: STAFFS
+         staffs: STAFFS,
+         departments: DEPARTMENTS,
+         roles: ROLE
       }
    }
    render() {
-
+      const StaffWithId = (match) => {
+         return(
+            <StaffInfo staff={this.state.staffs.filter((staff) => staff.id === parseInt(match.params.staffId))[0]} />
+         )
+      }
       return (
          <div>
             <Header />
             <Switch>
                <Route exact path='/nhanvien' component={() => <HomePage staffs={this.state.staffs} />} />
-               
+               <Route path='/nhanvien/:staffId' component={StaffWithId} />
                <Redirect to='/nhanvien' />
             </Switch>
             <Footer />
